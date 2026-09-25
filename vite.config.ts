db6039b8e410ff533c8a70646dd911e9a53d2f05
mymaxbusiness11-hub <mymaxbusiness11@gmail.com>
2026-09-25 15:48:47 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  build: {
+    minify: "esbuild",
+  },
 });
